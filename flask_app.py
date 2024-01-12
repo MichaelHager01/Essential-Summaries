f1ce6cdf66c47fb32c495632d8a05384ba821577
Michael Hager <michaelhswe@gmail.com>
2024-01-12 09:55:13 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,10 @@
 from googleapiclient.http import MediaIoBaseUpload
-from googleapiclient.errors import HttpError
 import io
 from Google import Create_Service
 from flask import Flask, render_template, request
 import sys
+import json
+
 
 app = Flask(__name__)
 path = '/home/essentialsummaries/mysite'
@@ -11,8 +12,8 @@ path = '/home/essentialsummaries/mysite'
 if path not in sys.path:
    sys.path.insert(0, path)
 
-from flask_app import app as application
 
+# Creating folder for the google drive.
 def create_folder(service, folder_name, parent_folder_id=None):
     body = {
         'name': folder_name,
@@ -22,6 +23,7 @@ def create_folder(service, folder_name, parent_folder_id=None):
     folder = service.files().create(body=body).execute()
     return folder['id']
 
+# Uploading documents to google drive.
 def upload_to_google_drive(service, file_content, file_name, folder_id=None):
     media_body = MediaIoBaseUpload(io.BytesIO(file_content), mimetype='application/pdf', resumable=True)
     body = {
@@ -30,6 +32,7 @@ def upload_to_google_drive(service, file_content, file_name, folder_id=None):
     }
     service.files().create(body=body, media_body=media_body).execute()
     return
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -48,14 +51,17 @@ def index():
             return render_template('index.html', success_message=success_message)
 
         CLIENT_SECRET_FILE = '/home/essentialsummaries/mysite/client_secret_EssentialSummaries.json'
+        with open(CLIENT_SECRET_FILE, 'r') as json_file:
+            service_account_info = json.load(json_file)
+
         API_NAME = 'drive'
         API_VERSION = 'v3'
         SCOPES = ['https://www.googleapis.com/auth/drive']
 
-        service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+        service = Create_Service(service_account_info, API_NAME, API_VERSION, SCOPES)
 
         # Get the ID of the "Website Uploads" folder
-        website_uploads_folder_id = '1CgDFrdJ5NvO-EfIPpT3r99Ns2kxgz6yf'
+        website_uploads_folder_id = 'FOLDER_ID_SECRET'
 
         # Create a subfolder with the email as the folder name
         subfolder_id = create_folder(service, email, website_uploads_folder_id)
